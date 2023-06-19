fe19036f65e334918021700329792d73611d4c91
Guimg4689 <guilherme.martins20@hotmail.com>
2023-06-19 00:50:25 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,28 @@ public class Main {
         mentoria.setDescricao("Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+*/
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGuilherme = new Dev();
+        devGuilherme.setNome("Guilherme");
+        devGuilherme.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos: " + devGuilherme.getConteudosInscritos());
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        System.out.println("Conteudos Inscritos: " + devGuilherme.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos: " + devGuilherme.getConteudosConcluidos());
+        System.out.println(devGuilherme.calcularTotalXp());
 
 
     }
